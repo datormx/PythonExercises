04967136f68d6a0dbb543edc9605ccee1638737a
Miguel Torres <migueltorresio@outlook.com>
2020-04-27 17:36:13 -0500

    Still testing almostIncreasingSequence exercise. Trying a solution from a member of the community

--- almostIncreasingSequence.py
+++ almostIncreasingSequence.py
@@ -1,96 +1,38 @@
 def almostIncreasingSequence(sequence):
-	elements_removed = 0	
-	elements_removedb = 0	
-	sequenceb = sequence.copy()
-
-	for element in range(len(sequence)):
-		print(f'list: {sequence}')
-
-		for i in range(len(sequence)):					
-			if i < (len(sequence) - 1) and i > 0:
-				if sequence[i] < sequence[i - 1]:
-					elements_removed+= 1
-					print(f'x) To remove {sequence[i - 1]}')  
-					sequence.pop(i - 1)
-				elif sequence[i] > sequence[i - 1] and sequence[i] < sequence[i + 1]:
-					print('A) OK')
-				elif sequence[i] >= sequence[i - 1] and sequence[i] < sequence[i + 1]:
-					elements_removed+= 1
-					print(f'B) To remove {sequence[i]}')  
-					sequence.pop(i)
-				elif sequence[i] > sequence[i - 1] and sequence[i] < sequence[i + 1]:
-					elements_removed+= 1
-					print(f'C) To remove {sequence[i + 1]}')  
-					sequence.pop(i + 1)
-				elif sequence[i] > sequence[i - 1] and sequence[i] >= sequence[i + 1]:
-					elements_removed+= 1
-					print(f'D) To remove {sequence[i + 1]}')  
-					sequence.pop(i + 1)			
-
-	for element in range(len(sequenceb)):
-		print(f'list: {sequenceb}')
-
-		for i in range(len(sequenceb)):			
-			if i < (len(sequenceb) - 1) and i > 0:
-				if sequenceb[i] < sequenceb[i - 1]:
-					elements_removedb+= 1
-					print(f'x) To remove {sequenceb[i - 1]}')  
-					sequenceb.pop(i - 1)
-				elif sequenceb[i] > sequenceb[i - 1] and sequenceb[i] < sequenceb[i + 1]:
-					print('A) OK')
-				elif sequenceb[i] >= sequenceb[i - 1] and sequenceb[i] < sequenceb[i + 1]:
-					elements_removedb+= 1
-					print(f'B) To remove {sequenceb[i]}')  
-					sequenceb.pop(i)
-				elif sequenceb[i] > sequenceb[i - 1] and sequenceb[i] < sequenceb[i + 1]:
-					elements_removedb+= 1
-					print(f'C) To remove {sequenceb[i + 1]}')  
-					sequenceb.pop(i + 1)
-				elif sequenceb[i] > sequenceb[i - 1] and sequenceb[i] >= sequenceb[i + 1]:
-					elements_removedb+= 1
-					print(f'D) To remove {sequenceb[i]}')  
-					sequenceb.pop(i)
-
-
-	if elements_removed > elements_removedb:
-		if elements_removedb <= 1:
-			print(f'Total Elements removed: {elements_removedb}')
-			print(f'Final list: {sequenceb}')
-			return True
-		else:
-			print(f'Total Elements removed: {elements_removedb}')
-			print(f'Final list: {sequenceb}')
-			return False
-
-	elif elements_removedb <= elements_removedb:
-		if elements_removed <= 1:
-			print(f'Total Elements removed: {elements_removed}')
-			print(f'Final list: {sequence}')
-			return True
-		else:
-			print(f'Total Elements removed: {elements_removed}')
-			print(f'Final list: {sequence}')
-			return False	
-
+    droppped = False
+    last = prev = min(sequence) - 1
+    for elm in sequence:
+        if elm <= last:
+            if droppped:
+                return False
+            else:
+                droppped = True
+            if elm <= prev:
+                prev = last
+            elif elm >= prev:
+                prev = last = elm
+        else:
+            prev, last = last, elm
+    return True
 
 if __name__ == "__main__":
-	# sequence = [1, 1, 2, 3, 4, 4] #False
+	sequence = [1, 1, 2, 3, 4, 4] #False
 	# sequence = [1, 2, 1, 2] #False
-	# sequence = [1, 3, 2, 1] #False
+	sequencea = [1, 3, 2, 1] #False
 	# sequence = [1, 2, 3, 4, 3, 6] #True
-	sequence = [1, 2, 5, 3, 5] #True
-	sequence2 = [1, 2, 3, 4, 3, 6] #True
-	sequence3 = [10, 1, 2, 3, 4, 5, 6, 1] #False
-	sequence4 = [1000, 1000, 2000, 3000, 4000, 5000, 5000]
-	sequence5 = [6, 5, 4]
-
+	# sequence = [1, 2, 5, 3, 5] #True
+	# sequence2 = [1, 2, 3, 4, 3, 6] #True
+	# sequence3 = [10, 1, 2, 3, 4, 5, 6, 1] #False
+	# sequence4 = [1000, 1000, 2000, 3000, 4000, 5000, 5000]
+	# sequence5 = [6, 5, 4] #False
+	# sequence6 = [-9996, -9995, -9994, -9993, -9991, -9989, -9987, -9986, -9985, -9983, -9982, -9980, -9978, -9977, -9976, -9975, -9974, -9972, -9968, -9966, -9965, -9961, -9957, -9956, -9955, -9954, -9952, -9948, -9942, -9939, -9938, -9936, -9935, -9932, -9931, -9927, -9925, -9923, -9922, -9921, -9920, -9919, -9918, -9908, -9905, -9902, -9901, -9900, -9899, -9897, -9896, -9894, -9888, -9886, -9880, -9878, -9877, -9876, -9874, -9872, -9871, -9870, -9869, -9868, -9867, -9865, -9857, -9856, -9855, -9854, -9853, -9852, -9851, -9849, -9848, -9846, -9845, -9843, -9842, -9841, -9840, -9837, -9834, -9828, -9826, -9824, -9823, -9820, -9816, -9814, -9812, -9811, -9810, -9809, -9807, -9806, -9804, -9803, -9801, -9800]
+	sequence7 = [5, 5, 5] #False
+	sequence8 = [10, 1, 2, 3, 4, 5] #True
 
 	print(almostIncreasingSequence(sequence))
-	print(almostIncreasingSequence(sequence2))
-	print(almostIncreasingSequence(sequence3))
-	print(almostIncreasingSequence(sequence4))
-	print(almostIncreasingSequence(sequence5))
-	
+	print(almostIncreasingSequence(sequencea))
+	print(almostIncreasingSequence(sequence7))
+	print(almostIncreasingSequence(sequence8))
 
 	
 

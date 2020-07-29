69692e7d8c8086bf48c679e0d384ada4ccdf4235
Miguel Torres <migueltorresio@outlook.com>
2020-07-29 16:04:21 -0500

    Se agrega #PlatziCodingChallenge Día 19 y 20: Memory (Parte 2 y 3)

--- Platzi100Days/memory.py
+++ Platzi100Days/memory.py
@@ -1,11 +1,33 @@
-from random import choices
+import random
 
 CARACTERES = ['\"', '#', '$', '%', '(', ')', '/', '&']
 
-seleccion = choices(CARACTERES, k = 4)
+seleccion = random.choices(CARACTERES, k = 4)
 
-simbolos = ''
-for char in seleccion:
-    simbolos += char
+simbolos = {}
+for i, char in enumerate(seleccion):
+    simbolos[i] = char
 
-print(simbolos)
+simbolos_desordenados = list(simbolos.values())
+random.shuffle(simbolos_desordenados)
+simbolos_desordenados = ''.join(simbolos_desordenados)
+
+# print(simbolos) #Scaffolding
+
+print(f'Los símbolos fueron: {simbolos_desordenados}. Adivina el orden en que se ingresaron.')
+
+simbolos_elegidos = {}
+while simbolos_elegidos != simbolos:    
+
+    simbolos_ingresados = input('\nIngresa todos los simbolos en el orden: ')
+    
+    for i, char in enumerate(simbolos_ingresados):
+        simbolos_elegidos[i] = char
+    
+    if simbolos_elegidos != simbolos:
+        print('\tIncorrecto. Intenta de nuevo.')
+
+print(f'¡Felicidades! El orden es {simbolos}.')
+
+
+

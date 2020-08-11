2705bb0945dc36419230d27305df8b6291d442ae
Miguel Torres <migueltorresio@outlook.com>
2020-08-10 21:06:51 -0500

    Se agrega #PlatziCodingChallenge Días 25, 26 y 27 - Ordenando ando

---
+++ Platzi100Days/ordenando_ando/ordenando_ando_1.py
@@ -0,0 +1,67 @@
+import time
+import csv
+
+def read_csv():
+    with open('datos.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ',')  
+
+        return list(csv_reader)
+
+
+def ordenamiento_burbuja(lista):
+    n = len(lista)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+    return lista
+    
+
+def ordenamiento_insercion(lista):
+    for indice in range(1, len(lista)):
+        valor_actual = lista[indice]
+        posicion_actual = indice
+
+        while posicion_actual > 0 and lista[posicion_actual - 1] > valor_actual:
+            lista[posicion_actual] = lista[posicion_actual - 1]
+            posicion_actual -= 1
+
+        lista[posicion_actual] = valor_actual
+
+    return lista
+
+
+def run():
+    #Ordenamiento de burbuja
+    comienzo = time.time()
+
+    nombres = read_csv()
+    print(f'Nombres: {nombres}')
+    nombres_ordenados = ordenamiento_burbuja(nombres)
+    print(f'\n\nNombres ordenados: {nombres_ordenados}')
+
+    final = time.time()
+
+    tiempo = final - comienzo
+    print(f'\nDuración ordenamiento de burbuja: {tiempo} segundos')
+
+
+    #Ordenamiento por inserción
+
+    comienzo = time.time()
+
+    nombres = read_csv()
+    print(f'Nombres: {nombres}')
+    nombres_ordenados = ordenamiento_insercion(nombres)
+    print(f'\n\nNombres ordenados: {nombres_ordenados}')
+
+    final = time.time()
+
+    tiempo = final - comienzo
+    print(f'\nDuración ordenamiento por inserción: {tiempo} segundos')
+
+if __name__ == "__main__":
+    run()

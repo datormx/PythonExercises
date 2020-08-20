44ae8c32947144c1eae8c05ab9a46fc2b379adb9
Miguel Torres <migueltorresio@outlook.com>
2020-08-19 21:46:25 -0500

    Se agrega #PlatziCodingChallenge Día 34 - Números primos

--- Platzi100Days/numeros_primos.py
+++ Platzi100Days/numeros_primos.py
@@ -1,54 +1,42 @@
-def calcular_primos(numero_max):
-    """
-    Calcula una serie de números primos hasta el número máximo indicado.
+def prime_numbers(n, memory = {}):
+    number = 0
+    position = 1
+    while len(memory) < n:
+        result = is_prime(number, memory)
+        if result:
+            memory[number] = position
+            position += 1
+        number += 1        
 
-    Parámetros:
-    numero max int > 0
-
-    Retorna:
-    Lista con serie de numeros primos
-    """
-    numeros = [num for num in range(numero_max + 1)]
-    primos = []
-
-    for num in numeros:
-        primo = is_primo(num)
-        if primo:
-            primos.append(num)                
-    
-    return primos
+    return memory
 
 
-def is_primo(numero):
-    """Recibe un número entero y determina si es primo
+def is_prime(number, memory):
+    count = 0
 
-    Parámetros:
-    numero int > 0
+    if number <= 1:
+        return False    
 
-    retorna:
-    True or False"""
-
-    if numero < 2:
-        return False
-    elif numero == 2:
+    for i in range(1, number + 1):     
+        if i == 1 or i == number:
+            continue   
+        if number % i == 0:
+            count += 1
+            break
+    if count == 0:
         return True
-    elif numero > 2 and numero % 2 == 0:
+    else:
         return False
-    else: 
-        for i in range(3, numero):
-            if numero % i == 0:
-                return False
-    return True
 
 
-def main():
-    print('\nEscribe un número para listar todos los números primos hasta ese número.')
-    limite = int(input('\nIngresa el número máximo: '))
-    numeros_primos = calcular_primos(limite)
-    print(f'\nNúmeros primos del 1 hasta el {limite}:')
-    for numero in numeros_primos:
-        print(numero)
+def run():
+
+    n = int(input('Insert a number: '))
+    results = prime_numbers(n)
+    print(results)
 
 
 if __name__ == "__main__":
-    main()
+    run()
+
+
